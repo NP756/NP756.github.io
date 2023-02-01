aeadddb6c9206d75a197976a15696bfc59aad8cc
NP756 <98428368+NP756@users.noreply.github.com>
2023-01-31 20:43:19 -0500

    Update hw1_2.py

--- hw1_2.py
+++ hw1_2.py
@@ -1,5 +1,5 @@
-a = float(input("Enter Homework Grade: "))
-b = float(input("Enter Midterm Grade: "))
-c = float(input("Enter Final Exam Grade: "))
+a = float(input("Enter Homework grade: "))
+b = float(input("Enter Midterm grade: "))
+c = float(input("Enter Final Exam grade: "))
 finalGrade = (a*0.45)+(b*0.25)+(c*0.30)
 print("Your final grade is: ", round(finalGrade, 2))
