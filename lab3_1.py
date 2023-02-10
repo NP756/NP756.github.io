1e050d3c98e154f487c9343364ef92b4d8386163
NP756 <98428368+NP756@users.noreply.github.com>
2023-02-10 08:47:42 -0500

    Create lab3_1.py

---
+++ lab3_1.py
@@ -0,0 +1,31 @@
+totalList = []
+warningList = []
+safeList = []
+dangerList = []
+
+i = 0
+
+warningValue = int(input("Enter threshold for warning values: "))
+dangerValue = int(input('Enter threshold for danger values: "))
+
+while i >= 0:
+    vibration = float(int("Enter vibration value (-1 to quit): "))
+        totalList.append(vibration)
+        if totalList[i] < warningValue:
+            warningList.append(vibration[i])
+        if totalList[i] > dangerValue:
+            dangerList.append(vibration[i])
+        if totalList[i] > warningValue && totalList[i] < dangerValue:
+            safeList.append[vibration[i])
+        if vibration = -1:
+            i = -1
+
+print("There are safe readings: ")
+print(safeList)
+                            
+print("There are warning readings: ")
+print(warningList)
+
+print("There are danger readings: ")
+print(dangerList)
+                            
